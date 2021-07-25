af976de8901ba560da4f88207009dea0825026e5
Jakub Tomczak <jakub.tomczak@live.com>
2021-07-25 12:54:27 +0200

    Fix alternatives names in the monotonicity constraints

--- ror/monotonicity_constraints.py
+++ ror/monotonicity_constraints.py
@@ -2,11 +2,20 @@ from ror.Relation import Relation
 import numpy as np
 from typing import List, Dict
 from ror.Constraint import Constraint, ConstraintVariable, ConstraintVariablesSet
-from ror.Dataset import Dataset, criterion_types
+from ror.Dataset import Dataset
 
 
 def create_monotonicity_constraints(dataset: Dataset) -> Dict[str, List[Constraint]]:
+    '''
+    Sort each criterion in descending order. Assume best value is on the 0th index,
+    create constraints by taking all alternatives and comparing it with the best value.
+    If values are [a1:0, a2:6, a3: 5, a4: 10], then they will be sorted to
+    [a4: 10, a2: 6, a3: 5, a1: 0], and there will be 3 constraints returned, starting from
+    u1(a4) >= u1(a2) and then normalized to -u1(a4) + u1(a2) <= 0
+    '''
     assert dataset is not None, "dataset cannot be None"
+    assert len(
+        dataset.alternatives) > 1, "number of alternatives in the dataset must be greater than 1"
     data = dataset.matrix
     criteria = dataset.criteria
     constraints = dict()
@@ -22,24 +31,23 @@ def create_monotonicity_constraints(dataset: Dataset) -> Dict[str, List[Constrai
         _data_indices = _data_indices[::-1]
 
         best_value_index = _data_indices[0]
-        # iterate over all values, skipping the best (first) value
-        # for row, row_index in zip(_data[1:], _data_indices[1:]):
+        # iterate over all alternatives' values in the criterion,
+        # skipping the best (first) value
         for row_index in _data_indices[1:]:
+            row_variable_name = Constraint.create_variable_name(
+                'u', criterion_name, dataset.alternatives[row_index])
+            best_variable_name = Constraint.create_variable_name(
+                'u', criterion_name, dataset.alternatives[best_value_index])
             _constraints.append(
-                Constraint(ConstraintVariablesSet([
-                    ConstraintVariable(
-                        Constraint.create_variable_name(
-                            'u', criterion_name, row_index),
-                        -1,
-                    ),
-                    ConstraintVariable(
-                        Constraint.create_variable_name(
-                            'u', criterion_name, best_value_index),
-                        -1,
-                    )
-                ]),
+                Constraint(
+                    ConstraintVariablesSet([
+                        ConstraintVariable(row_variable_name, 1.0,
+                                           dataset.alternatives[row_index]),
+                        ConstraintVariable(
+                            best_variable_name, -1.0, dataset.alternatives[best_value_index])
+                    ]),
                     Relation('<=', 'monotonicity relation'),
-                    f"mono_{Constraint.create_variable_name('u', criterion_name, row_index)}_{Constraint.create_variable_name('u', criterion_name, best_value_index)}"
+                    f"mono_{best_variable_name}_{row_variable_name}"
                 )
             )
         constraints[criterion_name] = _constraints
---
+++ tests/test_monotonicity_constraints.py
@@ -0,0 +1,88 @@
+from ror.Dataset import read_dataset_from_txt
+import unittest
+from ror.monotonicity_constraints import create_monotonicity_constraints
+
+
+class TestMonotonicityConstraints(unittest.TestCase):
+    def test_creating_monotonicity_constraints_failed(self):
+        with self.assertRaises(AssertionError):
+            create_monotonicity_constraints(None)
+
+    def test_creating_monotonicity_constraints_success(self):
+        data = read_dataset_from_txt("tests/datasets/example2.txt")
+
+        constraints = create_monotonicity_constraints(data)
+
+        self.assertEqual(len(constraints), 2)
+        self.assertTrue("gain criterion" in constraints)
+        self.assertTrue("cost criterion" in constraints)
+
+        # gain criterion
+        gain_criterion_constraints = constraints['gain criterion']
+
+        self.assertEqual(len(gain_criterion_constraints), 3)
+
+        # first constraint for gain criterion
+        self.assertEqual(
+            gain_criterion_constraints[0].name, "mono_u_gain criterion_a4_u_gain criterion_a2")
+        best_value_first_constraint_first_criterion = gain_criterion_constraints[0].get_variable(
+            'u_gain criterion_a4')
+        self.assertIsNotNone(best_value_first_constraint_first_criterion)
+        self.assertAlmostEqual(
+            best_value_first_constraint_first_criterion.coefficient, -1.0)
+
+        worst_value_first_constraint_first_criterion = gain_criterion_constraints[0].get_variable(
+            'u_gain criterion_a2')
+        self.assertIsNotNone(worst_value_first_constraint_first_criterion)
+        self.assertAlmostEqual(
+            worst_value_first_constraint_first_criterion.coefficient, 1.0)
+
+        # last constraint for gain criterion
+        self.assertEqual(
+            gain_criterion_constraints[2].name, "mono_u_gain criterion_a4_u_gain criterion_a1")
+        best_value_third_constraint_first_criterion = gain_criterion_constraints[2].get_variable(
+            'u_gain criterion_a4')
+        self.assertIsNotNone(best_value_third_constraint_first_criterion)
+        self.assertAlmostEqual(
+            best_value_third_constraint_first_criterion.coefficient, -1.0)
+
+        worst_value_third_constraint_first_criterion = gain_criterion_constraints[2].get_variable(
+            'u_gain criterion_a1')
+        self.assertIsNotNone(worst_value_third_constraint_first_criterion)
+        self.assertAlmostEqual(
+            worst_value_third_constraint_first_criterion.coefficient, 1.0)
+
+        # cost criterion
+        cost_criterion_constraints = constraints['cost criterion']
+
+        self.assertEqual(len(cost_criterion_constraints), 3)
+
+        # first constraint for cost criterion
+        self.assertEqual(
+            cost_criterion_constraints[0].name, "mono_u_cost criterion_a4_u_cost criterion_a1")
+        best_value_first_constraint_second_criterion = cost_criterion_constraints[0].get_variable(
+            'u_cost criterion_a4')
+        self.assertIsNotNone(best_value_first_constraint_second_criterion)
+        self.assertAlmostEqual(
+            best_value_first_constraint_second_criterion.coefficient, -1.0)
+
+        worst_value_first_constraint_first_criterion = cost_criterion_constraints[0].get_variable(
+            'u_cost criterion_a1')
+        self.assertIsNotNone(worst_value_first_constraint_first_criterion)
+        self.assertAlmostEqual(
+            worst_value_first_constraint_first_criterion.coefficient, 1.0)
+
+        # first constraint for cost criterion
+        self.assertEqual(
+            cost_criterion_constraints[2].name, "mono_u_cost criterion_a4_u_cost criterion_a2")
+        best_value_third_constraint_second_criterion = cost_criterion_constraints[2].get_variable(
+            'u_cost criterion_a4')
+        self.assertIsNotNone(best_value_third_constraint_second_criterion)
+        self.assertAlmostEqual(
+            best_value_third_constraint_second_criterion.coefficient, -1.0)
+
+        worst_value_third_constraint_first_criterion = cost_criterion_constraints[2].get_variable(
+            'u_cost criterion_a2')
+        self.assertIsNotNone(worst_value_third_constraint_first_criterion)
+        self.assertAlmostEqual(
+            worst_value_third_constraint_first_criterion.coefficient, 1.0)
