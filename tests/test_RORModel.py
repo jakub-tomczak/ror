3be4bdb691b8fbfe91eb65ffa1eba48e57f6c0c5
Jakub Tomczak <jakub.tomczak@live.com>
2021-07-30 12:19:52 +0200

    Test creating gurobi model

--- tests/test_RORModel.py
+++ tests/test_RORModel.py
@@ -1,6 +1,7 @@
 from ror.data_loader import read_dataset_from_txt
 import unittest
 from ror.RORModel import RORModel
+from gurobipy import GRB
 
 
 class TestRORModel(unittest.TestCase):
@@ -18,5 +19,23 @@ class TestRORModel(unittest.TestCase):
         self.assertEqual(len(model.constraints), 171)
         self.assertEqual(model.target, "delta")
 
+        model.to_gurobi_model()
+
+    def test_saving_model_to_lp(self):
+        data = read_dataset_from_txt("tests/datasets/ror_dataset.txt")
+        model = RORModel(data, 0.0, "Model with alpha 0.0")
+
+        self.assertEqual(len(model.constraints), 171)
+        self.assertEqual(model.target, "delta")
+
+        model.save_model()
+
+    def test_solving_model(self):
+        data = read_dataset_from_txt("tests/datasets/ror_full_dataset.txt")
+        model = RORModel(data, 0.0, "Model with alpha 0.0")
+
         gurobi_model = model.to_gurobi_model()
-        print(gurobi_model)
+        gurobi_model.optimize()
+
+        self.assertEqual(gurobi_model.status, GRB.OPTIMAL)
+        self.assertAlmostEqual(gurobi_model.objVal, 0.0)
