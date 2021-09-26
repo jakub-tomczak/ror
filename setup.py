1e833e4e992f85cce51c2fb6d21c167ae28aee09
Jakub Tomczak <jakub.tomczak@live.com>
2021-09-26 22:34:49 +0200

    Add setup files

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding = 'utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name = 'ror',
+  packages = ['ror'],
+  version = '0.0.1',
+  python_requires = '>=3.6',
+  license = 'MIT',
+  description = 'ROR method solver',
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
+  author = 'Jakub Tomczak',
+  url = 'https://github.com/jtomczak/ror',
+  keywords = ['tkinter', 'table', 'widget'],
+  install_requires = [],
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Programming Language :: Python :: 3',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+)
