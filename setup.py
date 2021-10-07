f82ee909f890f9f0b809228cd9a544d3071c7ba1
Jakub Tomczak <jakub.tomczak@live.com>
2021-10-07 12:58:17 +0200

    Remove reading requirements in the setup script

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding = 'utf-8') as f:
     long_description = f.read()
 
-# with open(path.join(this_directory, 'requirements.txt'), encoding = 'utf-8') as f:
-#     long_description = f.read()
-
 setup(
   name = 'ror',
   packages = ['ror'],
