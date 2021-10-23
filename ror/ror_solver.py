7adfa230635561e4b3f187a69caf9f9f67657a4f
Jakub Tomczak <jakub.tomczak@live.com>
2021-10-23 10:56:22 +0200

    Refactor ror solver

--- ror/ror_solver.py
+++ ror/ror_solver.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 import logging
 from typing import Callable, Dict
+from ror.BordaResultAggregator import BordaResultAggregator
 from ror.Constraint import ConstraintVariable, ConstraintVariablesSet
 from ror.Dataset import RORDataset
 from ror.RORModel import RORModel
@@ -25,7 +26,12 @@ def solve_model(loaderResult: LoaderResult) -> RORResult:
 
 AVAILABLE_AGGREGATORS: Dict[str, AbstractResultAggregator] = {
     result_aggregator.name: result_aggregator
-    for result_aggregator in [DefaultResultAggregator(), WeightedResultAggregator()]
+    for result_aggregator
+    in [
+        DefaultResultAggregator(),
+        WeightedResultAggregator(),
+        BordaResultAggregator()
+    ]
 }
 
 def solve_model(
@@ -37,30 +43,42 @@ def solve_model(
         **aggregation_method_kwargs,
     ) -> RORResult:
     assert aggregation_method in AVAILABLE_AGGREGATORS, f'Invalid aggregator method name {aggregation_method}, available: [{", ".join(AVAILABLE_AGGREGATORS.keys())}]'
-    alpha_values = AlphaValues.from_list(parameters.get_parameter(RORParameter.ALPHA_VALUES))
-    # models to solve is the number of all models that needs to be solved by the solver
-    # used to calculate the total progress of calculations
-    models_to_solve = 1 + len(data.alternatives) * len(alpha_values.values) + 2
-    models_solved = 0
-    def report_progress(models_solved: int, description: str):
-        models_solved += 1
-        if progress_callback is not None:
-            progress_callback(ProcessingCallbackData(models_solved / models_to_solve, description))
-        return models_solved
 
-    # step 1
-    logging.info('Starting step 1')
-    model = RORModel(
+    initial_model = RORModel(
         data,
         parameters[RORParameter.INITIAL_ALPHA],
         f"ROR Model, step 1, with alpha {parameters[RORParameter.INITIAL_ALPHA]}"
     )
 
-    model.target = ConstraintVariablesSet([
+    initial_model.target = ConstraintVariablesSet([
         ConstraintVariable("delta", 1.0)
     ])
-    result = model.solve()
-    models_solved = report_progress(models_solved, 'Step 1')
+    aggregator = AVAILABLE_AGGREGATORS[aggregation_method]
+    # get alpha values depending on the result aggregator
+    alpha_values = aggregator.get_alpha_values(initial_model, parameters)
+
+    # for raporting progress:
+    # Calculate number of steps to be solved.
+    # It consists of:
+    # 1. solving 1st model (initial model that verifies whether model is feasible)
+    # 2. solving all models (depends on the alpha value)
+    # 3. generating images for each rank
+    # 4. aggregating results
+    steps_to_solve = 1 + len(data.alternatives) * len(alpha_values.values) + 2
+    steps_solved = 0
+
+    # inner function for reporting calculations progress
+    def report_progress(models_solved: int, description: str):
+        models_solved += 1
+        if progress_callback is not None:
+            progress_callback(ProcessingCallbackData(models_solved / steps_to_solve, description))
+        return models_solved
+
+    # step 1
+    logging.info('Starting step 1')
+    
+    result = initial_model.solve()
+    steps_solved = report_progress(steps_solved, 'Step 1')
     logging.info(f"Solved step 1, delta value is {result.objective_value}")
 
     logging.info('Starting step 2')
@@ -68,6 +86,8 @@ def solve_model(
     data.delta = result.objective_value
 
     ror_result = RORResult()
+    # assign model here - this can be used later in result aggregator
+    ror_result.model = initial_model
     ror_result.alpha_values = alpha_values
     for alternative in data.alternatives:
         for alpha in alpha_values.values:
@@ -77,22 +97,19 @@ def solve_model(
             result = tmp_model.solve()
             assert result is not None, 'Failed to optimize the problem. Model is infeasible'
 
-            models_solved = report_progress(models_solved, f'Step 2, alternative: {alternative}, alpha {alpha}.')
+            steps_solved = report_progress(steps_solved, f'Step 2, alternative: {alternative}, alpha {alpha}.')
             
             ror_result.add_result(alternative, alpha, result.objective_value)
             logging.info(
                 f"alternative {alternative}, objective value {result.objective_value}")
 
-    models_solved = report_progress(models_solved, f'Aggregating results.')
-    # create new instance of aggregator
-    aggregator = AVAILABLE_AGGREGATORS[aggregation_method]
+    steps_solved = report_progress(steps_solved, f'Aggregating results.')
     final_result: RORResult = aggregator.aggregate_results(
         ror_result,
         parameters,
         *aggregation_method_args,
         **aggregation_method_kwargs
     )
-    final_result.model = model
     final_result.results_aggregator = aggregator
-    models_solved = report_progress(models_solved, 'Calculations done.')
+    steps_solved = report_progress(steps_solved, 'Calculations done.')
     return final_result
