bec3c1eb2f30db2b8e6cd49b3934dabc0e203095
Jakub Tomczak <jakub.tomczak@live.com>
2021-10-29 14:43:33 +0200

    Export constraints to the PDF or tex file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ror.data_loader import read_dataset_from_txt
 import logging
-from ror.latex_exporter import export_latex
+from ror.latex_exporter import export_latex, export_latex_pdf
 from ror.loader_utils import RORParameter
 from ror.ror_solver import solve_model
 
@@ -8,10 +8,15 @@ loading_result = read_dataset_from_txt("problems/buses.txt")
 data = loading_result.dataset
 parameters = loading_result.parameters
 chosen_aggregation_method = parameters.get_parameter(RORParameter.RESULTS_AGGREGATOR)
-print('chosen method', chosen_aggregation_method)
 
 result = solve_model(data, parameters, chosen_aggregation_method)
 
-export_latex(result.model, 'first model')
+try:
+    filename = export_latex_pdf(result.model, 'first model')
+    logging.info(f'Exported model\'s constraints to PDF latex file, path to exported file {filename}')
+except:
+    logging.info('Failed to export model\'s constraints to PDF latex file. Check if have pdflatex installed on your system.')
+    filename = export_latex(result.model, 'first model')
+    logging.info(f'Exported file to tex format, path to exported file {filename}')
 
 logging.info(f'Final rank is {result.final_rank}')
