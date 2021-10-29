b0ca273c94ea1f957e3c7608214123fd28902787
Jakub Tomczak <jakub.tomczak@live.com>
2021-10-29 12:26:35 +0200

    Test calculating created constraints

--- tests/test_RORModel.py
+++ tests/test_RORModel.py
@@ -1,3 +1,4 @@
+from typing import Set
 from ror.Constraint import ConstraintVariable, ConstraintVariablesSet
 from ror.data_loader import read_dataset_from_txt
 import unittest
@@ -9,8 +10,28 @@ class TestRORModel(unittest.TestCase):
         loading_result = read_dataset_from_txt("tests/datasets/ror_dataset.txt")
         data = loading_result.dataset
         model = RORModel(data, 0.0, "Model with alpha 0.0")
+        
+        reference_alternatives: Set[str] = set()
+        for relation in data.preferenceRelations:
+            reference_alternatives.update(relation.alternatives)
+        for intensity_relation in data.intensityRelations:
+            reference_alternatives.update(intensity_relation.alternatives)
+        inner_maximization_constraints = (3*len(data.criteria) + 1)* len(reference_alternatives)
 
-        self.assertEqual(len(model.constraints), 171)
+        for constraints_name in model.constraints_dict:
+            from ror.constraints_constants import ConstraintsName
+            if constraints_name == ConstraintsName.INNER_MAXIMIZATION.value:
+                self.assertEqual(len(model.constraints_dict[constraints_name]), inner_maximization_constraints)
+            elif constraints_name == ConstraintsName.PREFERENCE_INFORMATION.value:
+                self.assertEqual(len(model.constraints_dict[constraints_name]), 3)
+            elif constraints_name == ConstraintsName.PREFERENCE_INTENSITY_INFORMATION.value:
+                self.assertEqual(len(model.constraints_dict[constraints_name]), 1)
+            elif constraints_name == ConstraintsName.MIN_CONSTRAINTS.value:
+                self.assertEqual(len(model.constraints_dict[constraints_name]), len(data.criteria))
+            elif constraints_name == ConstraintsName.MAX_CONSTRAINTS.value:
+                self.assertEqual(len(model.constraints_dict[constraints_name]), 1)
+
+        self.assertEqual(len(model.constraints), 122)
         self.assertIsNone(model.target)
 
     def test_creating_gurobi_model_from_ror_model(self):
@@ -18,7 +39,7 @@ class TestRORModel(unittest.TestCase):
         data = loading_result.dataset
         model = RORModel(data, 0.0, "Model with alpha 0.0")
 
-        self.assertEqual(len(model.constraints), 171)
+        self.assertEqual(len(model.constraints), 122)
         model.target = ConstraintVariablesSet([
             ConstraintVariable("delta", 1.0)
         ])
@@ -30,7 +51,7 @@ class TestRORModel(unittest.TestCase):
         data = loading_result.dataset
         model = RORModel(data, 0.0, "Model with alpha 0.0")
 
-        self.assertEqual(len(model.constraints), 171)
+        self.assertEqual(len(model.constraints), 122)
         model.target = ConstraintVariablesSet([
             ConstraintVariable("delta", 1.0)
         ])
